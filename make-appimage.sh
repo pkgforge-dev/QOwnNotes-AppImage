50d70f4240bad9e13fa9d71480bbf3e9beec817d
Rabindra Dhakal <contact@qaidvoid.dev>
2026-03-28 09:42:17 +0545

    Init package

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q qownnotes | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/512x512/apps/QOwnNotes.png
+export DESKTOP=/usr/share/applications/PBE.QOwnNotes.desktop
 
-# Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/QOwnNotes /usr/share/QOwnNotes
 
-# Additional changes can be done in between here
-
-# Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
 quick-sharun --test ./dist/*.AppImage
